12dd0d1af0d42b2c75181fd9c52eb0b6080537f7
howardmann <howardmann27@gmail.com>
2017-03-20 00:53:06 +1100

    Add error catch if partial file does not exists

--- controllers/terminators.js
+++ controllers/terminators.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 let terminators = [
   {name: 'T1000'},
   {name: 'buzz lightyear'},
@@ -7,8 +9,10 @@ let terminators = [
 exports.index = function(req, res, next) {
   res.render('terminators/index', {
     data: terminators,
-    partial: function(){
-      return 'terminators/dynamic'
+    partial: function(name){
+      let path = 'views/partials/' + name + '.hbs';
+      if (!fs.existsSync(path)) { return 'terminators/404'}
+      return name;
     }
   });
 };
